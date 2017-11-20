1d3b6810e0ef3e32823d268e551604f0c8f9c197
Orestis Zambounis <orestisz@student.ethz.ch>
2017-11-20 16:38:46 +0100

    scripts for sceneNet dataset

--- object_segmentation/SceneNet/dataset.py
+++ object_segmentation/SceneNet/dataset.py
@@ -2,7 +2,6 @@ import os, sys
 import math
 import numpy as np
 import skimage.io
-import cv2
 from PIL import Image
 
 # Root directory of the project
@@ -15,24 +14,25 @@ from object_segmentation.object_config import Config
 import utils
 
 class ObjectsConfig(Config):
-    NAME = "seg_sceneNN"
+    NAME = "seg_SceneNet"
     # NAME = "seg_ADE20K"
 
     MODE = 'RGBD'
 
-    IMAGE_MIN_DIM = 512
-    IMAGE_MAX_DIM = 640
+    IMAGE_MIN_DIM = 256
+    IMAGE_MAX_DIM = 320
 
-    # IMAGES_PER_GPU = 2
+    IMAGES_PER_GPU = 2
+    # LEARNING_RATE = 0.002
     # LEARNING_RATE = 0.02
-
+    
     # Image mean (RGBD)
     MEAN_PIXEL = np.array([123.7, 116.8, 103.9, 1220.7])
 
 class ObjectsDataset(utils.Dataset):
-    def load_sceneNN(self, dataset_dir, subset):
+    def load_sceneNet(self, dataset_dir, subset):
         assert(subset == 'training' or subset == 'validation' or subset == 'testing')
-        dataset_dir = os.path.join(dataset_dir, subset)
+        dataset_dir = os.path.join(dataset_dir, subset, '0')
 
         # Add classes
         self.add_class("seg_sceneNN", 1, "object")
@@ -40,14 +40,17 @@ class ObjectsDataset(utils.Dataset):
         # Add images
         for i, (root, dirs, files) in enumerate(os.walk(dataset_dir)):
             root_split = root.split('/')
-            if root_split[-1] == 'image': # and subset in root_split:
+            if count > 0:
+                break
+            if root_split[-1] == 'photo': # and subset in root_split:
+                print('Loading {} data from {}'.format(subset, root_split[-2]))
                 for file in files:
                     parentRoot = '/'.join(root.split('/')[:-1])
-                    depth_path = os.path.join(parentRoot, 'depth', 'depth' + file[5:])
-                    mask_path = os.path.join(parentRoot, 'mask', 'mask_' + file)
+                    depth_path = os.path.join(parentRoot, 'depth', file[:-4] + '.png')
+                    instance_path = os.path.join(parentRoot, 'instance', file[:-4] + '.png')
                     # only add if corresponding mask exists
                     path = os.path.join(root, file)
-                    if os.path.isfile(depth_path) and os.path.isfile(mask_path):
+                    if os.path.isfile(depth_path) and os.path.isfile(instance_path):
                         if (os.stat(path).st_size):
                             im = Image.open(path)
                             width, height = im.size
@@ -56,7 +59,7 @@ class ObjectsDataset(utils.Dataset):
                                 image_id=i,
                                 path=path,
                                 depth_path=depth_path,
-                                mask_path=mask_path,
+                                instance_path=instance_path,
                                 width=width,
                                 height=height)
                             count += 1
@@ -84,25 +87,11 @@ class ObjectsDataset(utils.Dataset):
             one mask per instance.
         class_ids: a 1D array of class IDs of the instance masks.
         """
-        mask_path = self.image_info[image_id]['mask_path']
-        img = cv2.imread(mask_path, -1)
+        instance_path = self.image_info[image_id]['instance_path']
+        img = np.asarray(Image.open(instance_path))
 
-        R = img[:, :, 0]
-        G = img[:, :, 1]
-        B = img[:, :, 2]
-        A = img[:, :, 3]
-
-        # port to python from cpp script:
-        # https://github.com/scenenn/shrec17/blob/master/mask_from_label/mask_from_label.cpp
-        seg = np.bitwise_or(np.bitwise_or(np.bitwise_or(
-                np.left_shift(R, 24),
-                np.left_shift(G, 16)),
-                np.left_shift(B, 8)),
-                A)
-
-        # object_class_masks = (R.astype(np.uint16) / 10) * 256 + G.astype(np.uint16)
-        unique, unique_inverse = np.unique(seg.flatten(), return_inverse=True)
-        object_instance_masks = np.reshape(unique_inverse, seg.shape)
+        unique, unique_inverse = np.unique(img.flatten(), return_inverse=True)
+        object_instance_masks = np.reshape(unique_inverse, img.shape)
         instances = np.unique(unique_inverse).tolist()
         instances.remove(0)
         instance_masks = []
@@ -110,7 +99,7 @@ class ObjectsDataset(utils.Dataset):
             vfunc = np.vectorize(lambda a: 1 if a == instance else 0)
             instance_masks.append(vfunc(object_instance_masks))
         if not instance_masks:
-            raise ValueError("No instances for image {}".format(mask_path))
+            raise ValueError("No instances for image {}".format(instance_path))
         masks = np.stack(instance_masks, axis=2)
         class_ids = np.array([1] * len(instances), dtype=np.int32)
 
@@ -118,5 +107,5 @@ class ObjectsDataset(utils.Dataset):
 
 if __name__ == '__main__':
     dataset = ObjectsDataset()
-    dataset.load_sceneNN('/home/orestisz/data/ADE20K_2016_07_26', 'validation')
+    dataset.load_sceneNet('/external_datasets/SceneNet_RGBD', 'validation')
     masks, class_ids = dataset.load_mask(0)
---
+++ object_segmentation/SceneNet/train.py
@@ -0,0 +1,72 @@
+import os, sys
+import tensorflow as tf
+import re
+
+from dataset import *
+
+# Root directory of the project
+ROOT_DIR = os.path.abspath("../..")
+if ROOT_DIR not in sys.path:
+    sys.path.insert(0, ROOT_DIR)
+
+import model as modellib
+from model import log
+
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+# Path to COCO trained weights
+SCENENN_MODEL_PATH = os.path.join(ROOT_DIR, "logs/seg_scenenn20171114T0203/mask_rcnn_seg_scenenn_0190.h5")
+
+SCENENET_DIR = "/external_datasets/SceneNet_RGBD"
+
+config = ObjectsConfig()
+config.display()
+
+# Training dataset
+# dataset_train = ObjectsDataset()
+# dataset_train.load_sceneNN(SCENENET_DIR, "training")
+# dataset_train.prepare()
+
+# Validation dataset
+dataset_val = ObjectsDataset()
+dataset_val.load_sceneNN(SCENENET_DIR, "validation")
+dataset_val.prepare()
+
+# Create model in training mode
+print('creating model..')
+model = modellib.MaskRCNN(mode="training", config=config,
+                          model_dir=MODEL_DIR)
+
+exclude = []
+
+# # Which weights to start with?
+init_with = "scenenn"  # scenenn or last
+
+print('loading weights...')
+if init_with == "scenenn":
+    model.load_weights(SCENENN_MODEL_PATH, by_name=True,
+                   exclude=exclude)
+elif init_with == "last":
+    # Load the last model you trained and continue training
+    model.load_weights(model.find_last()[1], by_name=True)
+
+# ## Training
+# 
+# Train in two stages:
+# 1. Only the heads. Here we're freezing all the backbone layers and training only the randomly initialized layers (i.e. the ones that we didn't use pre-trained weights from MS COCO). To train only the head layers, pass `layers='heads'` to the `train()` function.
+# 
+# 2. Fine-tune all layers. For this simple example it's not necessary, but we're including it to show the process. Simply pass `layers="all` to train all layers.
+# Train the head branches
+# print('training heads...')
+# model.train(dataset_train, dataset_val, 
+#             learning_rate=config.LEARNING_RATE, 
+#             epochs=j, 
+#             layers='heads')
+
+# Fine tune all layers
+print('fine tuning all layers...')
+model.train(dataset_train, dataset_val, 
+            learning_rate=config.LEARNING_RATE,
+            epochs=1000,
+            layers='|'.join(exclude))
